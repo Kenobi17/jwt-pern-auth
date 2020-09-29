16e6965d57f8e4eec4c1c4b5ac108759edf0ff9b
Kenobi17 <matiascorrea420@gmail.com>
2020-09-29 20:19:06 -0300

    Finish dashboard route

--- client/src/routes/Dashboard.jsx
+++ client/src/routes/Dashboard.jsx
@@ -1,12 +1,18 @@
-import React, { Fragment, useContext } from "react";
+import React, { Fragment, useContext, useEffect } from "react";
 import { AuthenticationContext } from "../context/AuthenticationContext";
+import Logout from "../components/Logout";
 
 const Dashboard = () => {
-  const { setAuth } = useContext(AuthenticationContext);
+  const { user, getUser } = useContext(AuthenticationContext);
+
+  useEffect(() => {
+    getUser();
+    //eslint-disable-next-line
+  }, []);
   return (
     <Fragment>
-      <h1>Dashboard</h1>
-      <button onClick={() => setAuth(false)}>Log Out</button>
+      <h1>Dashboard {user.name}</h1>
+      <Logout />
     </Fragment>
   );
 };
