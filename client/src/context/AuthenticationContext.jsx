341897979a566b81422f3c421ffe002143eb8097
Kenobi17 <matiascorrea420@gmail.com>
2020-09-29 20:18:07 -0300

    Add getUser function and [user,setUser] state to AuthenticationContext.jsx

--- client/src/context/AuthenticationContext.jsx
+++ client/src/context/AuthenticationContext.jsx
@@ -1,16 +1,35 @@
 import React, { useState, createContext } from "react";
+import userAPI from "../apis/userAPI";
 
 export const AuthenticationContext = createContext();
 
 export const AuthenticationContextProvider = (props) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState({});
   const setAuth = (boolean) => {
     setIsAuthenticated(boolean);
+  };
+  const getUser = async () => {
+    try {
+      const response = await userAPI.get("/dashboard", {
+        headers: { token: localStorage.token },
+      });
+      setUser(response.data);
+    } catch (err) {
+      console.error(err.message);
+    }
   };
 
   return (
     <AuthenticationContext.Provider
-      value={{ isAuthenticated, setIsAuthenticated, setAuth }}>
+      value={{
+        isAuthenticated,
+        setIsAuthenticated,
+        user,
+        setUser,
+        setAuth,
+        getUser,
+      }}>
       {props.children}
     </AuthenticationContext.Provider>
   );
