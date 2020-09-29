18ea570f9438829663dae47b7cf6385a269b6cac
Kenobi17 <matiascorrea420@gmail.com>
2020-09-29 02:54:37 -0300

    Add login register and dashboard routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Login from "./routes/Login";
+import Register from "./routes/Register";
+import Dashboard from "./routes/Dashboard";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/dashboard" component={Dashboard} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
